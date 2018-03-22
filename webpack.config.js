5bb17f212dcc8588ad798134de2d304cf4235d08
Dorian Monnier <dorian.monnier@gmail.com>
2018-03-22 20:12:24 +0100

    Add lemon favicon and delete ie9 support

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/template.html',
+            favicon: './src/favicon.ico',
             filename: 'index.html'
         })
     ]
